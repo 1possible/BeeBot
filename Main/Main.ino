04bfcb1e468b469ce9ea3abe1ae33d1b711109a2
CorThibaut <99717761+CorThibaut@users.noreply.github.com>
2023-11-06 09:41:14 +0100

    add motor
    added motor and  and sensor

--- Main/Main.ino
+++ Main/Main.ino
@@ -8,6 +8,12 @@ int DIGITAL_OUT_R=3;
 int stepsL = 0;
 int stepsR = 0;
 int var = 0;
+int Time = 0;
+uint8_t speedR = 150;
+uint8_t speedL = 150;
+
+float TimValue = 960;
+float distance_mm = 0.0;
 
 const byte TRIGGER_PIN = 2; 
 const byte ECHO_PIN = 3;    
@@ -30,6 +36,14 @@ void setup() {
   pinMode(DIGITAL_OUT_L, INPUT);
   pinMode(DIGITAL_OUT_R, INPUT);
   
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = TimValue;                     // preload timer
+  TCCR1B |= (1 << CS10)|(1 << CS12);    // 1024 prescaler 
+  TIMSK1 |= (1 << TOIE1);      
+  interrupts();
+
   Serial.begin(9600);
 
   // attach Interrupt to Interrupt Service Routine
@@ -54,64 +68,40 @@ void setup() {
   pinMode(ECHO_PIN, INPUT);
 }
 
+ISR(TIMER1_OVF_vect)                    // interrupt service routine for overflow
+{
+  TCNT1 = value;                                // preload timer
+  timeTest ++;
+
+}
+
 void loop() {
-  uint8_t i;
 
-  digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
-  float distance_mm = measure / 2.0 * SOUND_SPEED;
-
+  measureDistance();
    switch(var){
-    case 0:
-      Serial.print("case 0");
+    case 10.0 ... 19.9:
+      LeftMotor->setSpeed(75);
+      RightMotor->setSpeed(75);
+      break;
+    case 0.0 ... 9.9::
+      LeftMotor->run(RELEASE);
+      RightMotor->run(RELEASE);
+      break;
+    case default :
+      LeftMotor->setSpeed(speedL);
+      RightMotor->setSpeed(speedR);
       LeftMotor->run(FORWARD);
       RightMotor->run(FORWARD);
-      for (i=0; i<255; i++) {
-        LeftMotor->setSpeed(i);
-        RightMotor->setSpeed(i);
-        delay(20);
-      }
-      var = 1;
-      break;
-    case 1:
-      Serial.print("case 1");
-      LeftMotor->run(BACKWARD);
-      RightMotor->run(FORWARD);
-      for (i=0; i<255; i++){
-        LeftMotor->setSpeed(75);
-        RightMotor->setSpeed(75);
-        delay(10);
-      }
-      var = 0;
-      break;
-    case 2:
-      Serial.print("case 3");
-      LeftMotor->run(FORWARD);
-      RightMotor->run(BACKWARD);
-      for (i=0; i<255; i++){
-        LeftMotor->setSpeed(75);
-        RightMotor->setSpeed(75);
-        delay(10);
-      }
-      var = 0;
-      break;
-    case 3:
-      Serial.print("case 3");
-      LeftMotor->run(BACKWARD);
-      RightMotor->run(BACKWARD);
-      for (i=0; i<255; i++){
-        LeftMotor->setSpeed(75);
-        RightMotor->setSpeed(75);
-        delay(10);
-      }
-      var = 0;
       break;
    }
 
-  Serial.print("end");
-  LeftMotor->run(RELEASE);
-  RightMotor->run(RELEASE);
-  delay(1000);
+}
+
+void measureDistance 
+{
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  unsigned long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+  distance_mm = measure / 2.0 * SOUND_SPEED;
 }
