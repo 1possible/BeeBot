120f0d2b9f72634833912e5803c2db415176ee7c
Emilie Van Bogaert <21258@ecam.be>
2024-02-20 00:02:12 +0100

    I2C
    la comm serial->arduino->I2C->esp->BLE fonctionne mais dans l'autre sens il y a encore des problème

---
+++ GreatRobot/GreatRobot-ESP/GreatRobot-ESP.ino
@@ -0,0 +1,126 @@
+#include "BluetoothSerial.h"
+//code exemple balise : https://github.com/TardyNoe/Balise/blob/main/Esp/BLEExample
+// https://www.aranacorp.com/fr/communication-bluetooth-avec-esp32/
+BluetoothSerial ESP_BT; // Object for Bluetooth
+//https://assiss.github.io/arduino-zhcn/cn/Tutorial/MasterReader.html
+//https://docs.espressif.com/projects/arduino-esp32/en/latest/api/i2c.html
+#include <Wire.h>
+
+const int I2C_SDA_PIN = 14;
+const int I2C_SCL_PIN = 27;
+const int I2C_Trig_PIN = 12;
+
+char buff[48];
+int tail_buff = 0;
+int head_buff = 0;
+
+bool error= false;
+
+void setup() {
+  Serial.begin(115200);
+  ESP_BT.begin("BEEBOT-ESP32_BT"); // Name of your Bluetooth signal
+  Serial.print("Bluetooth device is ready to pair \n");
+  pinMode(I2C_Trig_PIN,OUTPUT);
+  Wire.setPins(I2C_SDA_PIN,I2C_SCL_PIN);
+  Wire.begin(3);
+  Wire.onRequest(requestEvent); // register event
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+  //Serial.println("loop time\n");
+  /*if(Serial.available()){
+    //ESP_BT.write(Serial.read());
+    String dataReceived = Serial.readString();
+    
+    ESP_BT.print("Received:" + dataReceived);
+    
+  }*/
+  if (ESP_BT.available()) { // Check if we receive anything from Bluetooth
+    //Serial.write(ESP_BT.read());
+    String dataReceived = ESP_BT.readString(); // Read what we receive
+    ESP_BT.println("message recu");
+    for(int i=0; i < dataReceived.length()-2; i++){
+      buff[head_buff] = dataReceived[i];
+      increaseHead();
+      ESP_BT.println(head_buff);
+    }
+    digitalWrite(I2C_Trig_PIN,HIGH);
+    //ESP_BT.print("headbuff : ");
+    //ESP_BT.println(head_buff);
+    //ESP_BT.print("tailbuff : ");
+    //ESP_BT.println(tail_buff);
+    
+    /*Serial.println("Received:" + dataReceived); // Print what we receive
+    String dataReceived = ESP_BT.readString(); // Read what we receive
+    Serial.println("Received:" + dataReceived); // Print what we receive
+
+    // Extracting the float values
+    int firstCommaIndex = dataReceived.indexOf(',');
+    int secondCommaIndex = dataReceived.indexOf(',', firstCommaIndex + 1);
+
+    String firstValue = dataReceived.substring(0, firstCommaIndex);
+    String secondValue = dataReceived.substring(firstCommaIndex + 1, secondCommaIndex);
+    String thirdValue = dataReceived.substring(secondCommaIndex + 1);
+
+    float value1 = firstValue.toFloat();
+    float value2 = secondValue.toFloat();
+    float value3 = thirdValue.toFloat();
+
+    // Print the extracted values
+    Serial.print("Value 1: ");
+    Serial.println(value1);
+    Serial.print("Value 2: ");
+    Serial.println(value2);
+    Serial.print("Value 3: ");
+    Serial.println(value3);
+
+    // Send a response
+    ESP_BT.println(dataReceived);*/
+    
+  }
+  delay(20);
+}
+void increaseTail(){
+  tail_buff++;
+  if(tail_buff >= sizeof(buff)){
+    tail_buff = 0;
+  }
+}
+void increaseHead(){
+  head_buff++;
+  if(head_buff >= sizeof(buff)){
+    head_buff = 0;
+  }
+  if(head_buff == tail_buff){
+    error = true;
+    ESP_BT.println("error : overflow buffer");
+  }
+}
+void requestEvent()
+{
+  //Wire.write("hello ");
+  //ESP_BT.println("message demandé");
+  for(int i=0; i < 6; i++){
+    if(head_buff != tail_buff){
+      Wire.write(buff[tail_buff]); // respond with message of 6 bytes // as expected by master
+      ESP_BT.println(buff[tail_buff]);
+      increaseTail();
+      ESP_BT.println(tail_buff);
+    }/*else{
+      //Wire.write(" ");
+    }*/
+  }
+  if(head_buff == tail_buff){
+    digitalWrite(I2C_Trig_PIN,LOW);
+  }
+}
+void receiveEvent(int howMany)
+{
+  while(1 < Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read(); // receive byte as a character
+    ESP_BT.print(c);         // print the character
+  }
+  ESP_BT.println(); 
+}
--- GreatRobot/GreatRobot-Main/GreatRobot-Main.ino
+++ GreatRobot/GreatRobot-Main/GreatRobot-Main.ino
@@ -1,11 +1,64 @@
 //GreatRobot code
+#include <Wire.h>
+
+//sensor distance
+/*float distance_mm = 0.0;
+const byte TRIGGER_PIN = 7; 
+const byte ECHO_PIN = 6;   
+const unsigned long MEASURE_TIMEOUT = 25000UL;
+const float SOUND_SPEED = 340.0 / 1000;
+*/
+//esp BLE
+const byte EN_BLE_PIN = 8; 
+const int ESP_addrI2C = 3;
 
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(115200);
 
+  /*pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW);
+  pinMode(ECHO_PIN, INPUT);*/
+
+  pinMode(EN_BLE_PIN,INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  bool en_BLE = digitalRead(EN_BLE_PIN);
+  if(en_BLE == HIGH){
+    Serial.println("comm esp");
+    Wire.requestFrom(ESP_addrI2C,6);
+    while(Wire.available()){
+      char c = Wire.read();
+      Serial.print(c);
+    }
+    Serial.println();
+  }
+  if(Serial.available()){
+    Serial.print("en_pin");
+    Serial.println(en_BLE);
+    String dataReceived = Serial.readString();
+    Wire.beginTransmission(ESP_addrI2C);
+    for(int i=0; i < dataReceived.length(); i++){
+      Wire.write(dataReceived[i]);
+    }
+    Wire.endTransmission(); 
+  }
+    
+  /*measureDistance();
+  Serial.print(distance_mm);
+  Serial.println(" mm");*/
+  delay(50);
 
 }
+
+/*int measureDistance ()
+{
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  unsigned long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+  distance_mm = measure / 2.0 * SOUND_SPEED;
+  return distance_mm ; 
+}*/
