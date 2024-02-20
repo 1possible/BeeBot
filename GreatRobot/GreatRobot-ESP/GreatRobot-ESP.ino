3e3bb33d114065d9353880a251d3dce1de80b1f2
Emilie Van Bogaert <21258@ecam.be>
2024-02-20 12:01:28 +0100

    clear code

--- GreatRobot/GreatRobot-ESP/GreatRobot-ESP.ino
+++ GreatRobot/GreatRobot-ESP/GreatRobot-ESP.ino
@@ -14,9 +14,6 @@ const int I2C_SDA_PIN = 14;
 const int I2C_SCL_PIN = 27;
 const int I2C_Trig_PIN = 12;
 
-//char buff[48];
-//int tail_buff = 0;
-//int head_buff = 0;
 int dataToSend = 0;
 int lengthPayload = 6;
 
@@ -34,23 +31,10 @@ void setup() {
 }
 
 void loop() {
-  //Serial.println("loop time\n");
-  /*if(Serial.available()){
-    //ESP_BT.write(Serial.read());
-    String dataReceived = Serial.readString();
-    
-    ESP_BT.print("Received:" + dataReceived);
-    
-  }*/
   if (ESP_BT.available()) { // Check if we receive anything from Bluetooth
-    //Serial.write(ESP_BT.read());
     String dataReceived = ESP_BT.readString(); // Read what we receive
-    ESP_BT.println("message recu");
-    /*for(int i=0; i < dataReceived.length()-2; i++){
-      buff[head_buff] = dataReceived[i];
-      increaseHead();
-      ESP_BT.println(head_buff);
-    }*/
+    //ESP_BT.println("message recu");
+    // change the length of the data to be in the payload length
     int str_len = dataReceived.length();
     int str_modulo = str_len%6;
     if(str_modulo != 0){
@@ -59,80 +43,20 @@ void loop() {
         str_len++;
       }
     }
+    //change format of data
     char char_array[str_len];
     dataReceived.toCharArray(char_array, str_len);
-    Wire.slaveWrite((uint8_t *) char_array, str_len);
+    
+    Wire.slaveWrite((uint8_t *) char_array, str_len);//write the data in the I2C buffer
     dataToSend += str_len;
     digitalWrite(I2C_Trig_PIN,HIGH);
-    //ESP_BT.print("headbuff : ");
-    //ESP_BT.println(head_buff);
-    //ESP_BT.print("tailbuff : ");
-    //ESP_BT.println(tail_buff);
-    
-    /*Serial.println("Received:" + dataReceived); // Print what we receive
-    String dataReceived = ESP_BT.readString(); // Read what we receive
-    Serial.println("Received:" + dataReceived); // Print what we receive
-
-    // Extracting the float values
-    int firstCommaIndex = dataReceived.indexOf(',');
-    int secondCommaIndex = dataReceived.indexOf(',', firstCommaIndex + 1);
-
-    String firstValue = dataReceived.substring(0, firstCommaIndex);
-    String secondValue = dataReceived.substring(firstCommaIndex + 1, secondCommaIndex);
-    String thirdValue = dataReceived.substring(secondCommaIndex + 1);
-
-    float value1 = firstValue.toFloat();
-    float value2 = secondValue.toFloat();
-    float value3 = thirdValue.toFloat();
-
-    // Print the extracted values
-    Serial.print("Value 1: ");
-    Serial.println(value1);
-    Serial.print("Value 2: ");
-    Serial.println(value2);
-    Serial.print("Value 3: ");
-    Serial.println(value3);
-
-    // Send a response
-    ESP_BT.println(dataReceived);*/
-    
   }
-  delay(20);
+  //delay(20);
 }
-/*void increaseTail(){
-  tail_buff++;
-  if(tail_buff >= sizeof(buff)){
-    tail_buff = 0;
-  }
-}
-void increaseHead(){
-  head_buff++;
-  if(head_buff >= sizeof(buff)){
-    head_buff = 0;
-  }
-  if(head_buff == tail_buff){
-    error = true;
-    ESP_BT.println("error : overflow buffer");
-  }
-}*/
 void requestEvent()
 {
   //Wire.write("hello ");
   //ESP_BT.println("message demandé");
-  /*for(int i=0; i < 6; i++){
-    if(head_buff != tail_buff){
-      //Wire.slaveWrite((uint8_t *)buff[tail_buff],1); // respond with message of 6 bytes // as expected by master
-      Wire.print(buff[tail_buff]);
-      ESP_BT.println(buff[tail_buff]);
-      increaseTail();
-      ESP_BT.println(tail_buff);
-    }else{
-      Wire.print(" ");
-    }
-  }
-  if(head_buff == tail_buff){
-    digitalWrite(I2C_Trig_PIN,LOW);
-  }*/
   dataToSend = dataToSend - lengthPayload;
   if(dataToSend <= 0){
     digitalWrite(I2C_Trig_PIN,LOW);
