e94744d3aad0b747deb1b9f57411186c3ecb3abc
Emilie Van Bogaert <21258@ecam.be>
2024-02-22 18:14:31 +0100

    add motor and remove switch

--- GreatRobot/GreatRobot-Main/GreatRobot-Main.ino
+++ GreatRobot/GreatRobot-Main/GreatRobot-Main.ino
@@ -1,5 +1,10 @@
 //GreatRobot code
 #include <Wire.h>
+#include <Adafruit_MotorShield.h>
+//motor
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *MotorTwo = AFMS.getMotor(2);
+Adafruit_DCMotor *MotorFour = AFMS.getMotor(4);
 
 //sensor distance
 /*float distance_mm = 0.0;
@@ -14,7 +19,6 @@ unsigned long timeNow;
 
 //lcd
 int team;
-int state = 0;
 
 //esp BLE
 const byte EN_BLE_PIN = 8; 
@@ -24,6 +28,7 @@ String messageBLE = "";
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
+  AFMS.begin();
   Serial.begin(9600);
 
   /*pinMode(TRIGGER_PIN, OUTPUT);
@@ -31,7 +36,7 @@ void setup() {
   pinMode(ECHO_PIN, INPUT);*/
 
   pinMode(EN_BLE_PIN,INPUT);
-  state = WAIT;
+  state = TEAMCHOOSE;
 }
 
 void loop() {
@@ -45,24 +50,13 @@ void loop() {
     }
     msgInstru(messageBLE);
   }
-  /*if(Serial.available()){
-    Serial.print("en_pin");
-    Serial.println(en_BLE);
-    String dataReceived = Serial.readString();
-    Wire.beginTransmission(ESP_addrI2C);
-    for(int i=0; i < dataReceived.length(); i++){
-      Wire.write(dataReceived[i]);
-    }
-    Wire.endTransmission(); 
-  }*/
     
   /*measureDistance();
   Serial.print(distance_mm);
   Serial.println(" mm");*/
 
 
-  switch(state){
-  	case TEAMCHOOSE:
+  if(state == TEAMCHOOSE){
   	  String readString;
       String Q;
       delay(1);
@@ -76,38 +70,53 @@ void loop() {
         team = 1;
         state = WAIT;
         printBLE("YELLOW Team");
-        break;
+        //break;
       }
       //BLUE TEAM
       if (Q == "2"){
         team = 2;
         state = WAIT;
         printBLE("Blue Team");
-        break;
       }
-    case RUN:
+      //break;
+  }else if(state == RUN){
+      //printBLE("RUN cc");
       timeNow = millis()-timeStartRUN;
+      //printBLE("RUN "+String(timeNow)+" ms\n");
       if(timeNow >= 3000){
         state = END;
       }
-      break;
-    case END:
+  }else if(state == END){
       printBLE("stop motor");
-      state = WAIT;
-      break;
-      
+      MotorTwo->run(RELEASE); 
+      MotorFour->run(RELEASE);
+      state = TEAMCHOOSE;
+      //break;
   }
   delay(50);
 }
 
 void startRUNNING(){
 	state = RUN;
-    printBLE("start running");
-    timeStartRUN=millis();
+  printBLE("start running");
+  timeStartRUN=millis();
+  MotorTwo->setSpeed(250); 
+  MotorFour->setSpeed(250);
+  if(team ==1){
+    MotorTwo->run(FORWARD); 
+    MotorFour->run(FORWARD); 
+  }else{
+    MotorTwo->run(BACKWARD); 
+    MotorFour->run(BACKWARD);
+  }
 }
 void msgInstru(String msg){
   if(state == WAIT and msg.indexOf("start") != -1){
     startRUNNING();
+  }else if( msg.indexOf("state") != -1){
+    printBLE("state : "+String(state));
+  }else if( msg.indexOf("team") != -1){
+    printBLE("team : "+String(state));
   }
 }
 void printBLE(String msg){
