b92d8564b8e3c60d0a756735977d51dec5248c47
Emilie Van Bogaert <21258@ecam.be>
2024-03-22 12:31:54 +0100

    remove comm

--- GreatRobot/GreatRobot-Main/GreatRobot-Main.ino
+++ GreatRobot/GreatRobot-Main/GreatRobot-Main.ino
@@ -1,10 +1,10 @@
-//GreatRobot code
+ //GreatRobot code
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 //motor
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *RightMotor = AFMS.getMotor(2);
-Adafruit_DCMotor *LeftMotor = AFMS.getMotor(4);
+Adafruit_DCMotor *LeftMotor = AFMS.getMotor(3);
 int vL = 75; //Low speed left //75
 int VL = 250; //Hight speed left
 int vR = 75; //Low speed right //50
@@ -15,17 +15,9 @@ int v = 250; //speed forward
 #define IR_left_PIN 9
 #define IR_right_PIN 10
 
-//sensor distance
-/*float distance_mm = 0.0;
-const byte TRIGGER_PIN = 7; 
-const byte ECHO_PIN = 6;   
-const unsigned long MEASURE_TIMEOUT = 25000UL;
-const float SOUND_SPEED = 340.0 / 1000;
-*/
 enum { WAIT, RUN, END} state; 
 
 unsigned long timeNow;
-
 unsigned long timeStartRUN;
 unsigned long timeLine = 0;
 
@@ -38,27 +30,17 @@ void setup() {
   AFMS.begin();
   //serial (comm ard-lcd)
   Serial.begin(9600);
+  //Serial.print("setup...");
 
   //IR MOTOR
   pinMode(IR_left_PIN, INPUT);
   pinMode(IR_right_PIN, INPUT);
   
-  //test US sensor
-  /*pinMode(TRIGGER_PIN, OUTPUT);
-  digitalWrite(TRIGGER_PIN, LOW);
-  pinMode(ECHO_PIN, INPUT);*/
-
   //statemachine
   state = WAIT;
 }
 
 void loop() {
-    
-  /*measureDistance();
-  Serial.print(distance_mm);
-  Serial.println(" mm");*/
-
-
   switch(state){
     case WAIT:
     {
@@ -85,12 +67,14 @@ void loop() {
     case RUN:
     {
       timeNow = millis()-timeStartRUN;
-      if(timeNow >= 3000){
+      if(timeNow >= 120000){
         state = END;
       }
-      bool endline =followingLine();
-      if(endline){
-        state = END;
+      else{
+        bool endline =followingLine();
+        if(endline){
+          state = END;
+        }
       }
       break;
     }
@@ -99,6 +83,7 @@ void loop() {
       RightMotor->run(RELEASE); 
       LeftMotor->run(RELEASE);
       state = WAIT;
+      //Serial.println("END");
       break;
     }
   }
@@ -106,6 +91,7 @@ void loop() {
 }
 void startRUN(){
   state = RUN;
+  //Serial.println("RUN");
   timeStartRUN = millis();
 }
 
@@ -146,12 +132,3 @@ bool followingLine(){
   }
   return endLine;
 }
-/*int measureDistance ()
-{
-  digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-  unsigned long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
-  distance_mm = measure / 2.0 * SOUND_SPEED;
-  return distance_mm ; 
-}*/
