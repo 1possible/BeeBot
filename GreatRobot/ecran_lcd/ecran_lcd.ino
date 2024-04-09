ab90174891dc3d7af6410e505a08a05151ae1c2f
Emilie Van Bogaert <21258@ecam.be>
2024-04-09 10:32:48 +0200

    add menu plant + change team color

--- GreatRobot/ecran_lcd/ecran_lcd.ino
+++ GreatRobot/ecran_lcd/ecran_lcd.ino
@@ -13,7 +13,7 @@ const int TS_LEFT=915,TS_RT=195,TS_TOP=949,TS_BOT=196;
 
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
-Adafruit_GFX_Button red_btn, blue_btn, back_btn;
+Adafruit_GFX_Button red_btn, blue_btn, back_btn, pot_0_btn, pot_1_btn, pot_2_btn,pot_3_btn, pot_4_btn;
 
 int pixel_x, pixel_y;     //Touch_getXY() updates global vars
 bool Touch_getXY(void)
@@ -63,12 +63,14 @@ int state = 0;
 
 void setup(void)
 {
+  Serial.begin(9600);
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
   uint16_t ID = tft.readID();
   if (ID == 0xD3D3) ID = 0x9486; // write-only shield
   tft.begin(ID);
   tft.setRotation(0);            //PORTRAIT
+  
 
 }
 
@@ -83,29 +85,84 @@ void loop(void)
       tft.fillScreen(BLACK);
       //print message
       tft.setTextSize(4);
-      tft.setCursor(0, 200);
+      tft.setCursor(50, 100);
       tft.setTextColor(WHITE);
-      tft.println("Choose a Team");
+      tft.println("number of");
+      tft.setCursor(100, 130);
+      tft.println("plant");
       //button
-      red_btn.initButton(&tft,  100, 260, 100, 40, WHITE, CYAN, BLACK, "RED", 2);
-      blue_btn.initButton(&tft, 220, 260, 100, 40, WHITE, CYAN, BLACK, "BLUE", 2);
-      red_btn.drawButton(false);
-      blue_btn.drawButton(false);
+      pot_1_btn.initButton(&tft,  100, 220, 100, 40, WHITE, CYAN, BLACK, "1", 2);
+      pot_2_btn.initButton(&tft,  220, 220, 100, 40, WHITE, CYAN, BLACK, "2", 2);
+      pot_3_btn.initButton(&tft,  100, 270, 100, 40, WHITE, CYAN, BLACK, "3", 2);
+      pot_4_btn.initButton(&tft,  220, 270, 100, 40, WHITE, CYAN, BLACK, "4", 2);
+      pot_0_btn.initButton(&tft,  160, 320, 100, 40, WHITE, CYAN, BLACK, "0", 2);
+      pot_0_btn.drawButton(false);
+      pot_1_btn.drawButton(false);
+      pot_2_btn.drawButton(false);
+      pot_3_btn.drawButton(false);
+      pot_4_btn.drawButton(false);
       state = 1;
       break;   
     }
     case 1 :
     {
       bool down = Touch_getXY();
+      pot_0_btn.press(down && pot_0_btn.contains(pixel_x, pixel_y));
+      pot_1_btn.press(down && pot_1_btn.contains(pixel_x, pixel_y));
+      pot_2_btn.press(down && pot_2_btn.contains(pixel_x, pixel_y));
+      pot_3_btn.press(down && pot_3_btn.contains(pixel_x, pixel_y));
+      pot_4_btn.press(down && pot_4_btn.contains(pixel_x, pixel_y));
+
+      if (pot_0_btn.justPressed()) {
+        state = 2;
+        break;
+      }
+      if (pot_1_btn.justPressed()) {
+        state = 2;
+        break;
+      }
+      if (pot_2_btn.justPressed()) {
+        state = 2;
+        break;
+      }
+      if (pot_3_btn.justPressed()) {
+        state = 2;
+        break;
+      }
+      if (pot_4_btn.justPressed()) {
+        state = 2;
+        break;
+      }
+      break;
+    }
+    case 2 :
+    {
+      tft.fillScreen(BLACK);
+      //print message
+      tft.setTextSize(4);
+      tft.setCursor(0, 200);
+      tft.setTextColor(WHITE);
+      tft.println("Choose a Team");
+      //button
+      red_btn.initButton(&tft,  100, 260, 100, 40, WHITE, CYAN, BLACK, "yellow", 2);
+      blue_btn.initButton(&tft, 220, 260, 100, 40, WHITE, CYAN, BLACK, "BLUE", 2);
+      red_btn.drawButton(false);
+      blue_btn.drawButton(false);
+      state = 3;
+      break;   
+    }
+    case 3 :
+    {
+      bool down = Touch_getXY();
       red_btn.press(down && red_btn.contains(pixel_x, pixel_y));
       blue_btn.press(down && blue_btn.contains(pixel_x, pixel_y));
 
       if (red_btn.justPressed()) {
-        tft.fillScreen(RED);
+        tft.fillScreen(YELLOW);
         //print message
         tft.setTextSize(4);
         tft.setCursor(50, 100);
-        tft.setTextColor(WHITE);
+        tft.setTextColor(BLACK);
         tft.println("Estimated");
         tft.setCursor(90, 130);
         tft.println("score");
@@ -114,10 +171,8 @@ void loop(void)
         tft.println(Score);
         digitalWrite(12, HIGH);
 
-        Serial.begin(9600);
         Serial.print("1");
-        Serial.end();
-        state = 2;
+        state = 4;
         break;
       }
       if (blue_btn.justPressed()) {
@@ -134,23 +189,21 @@ void loop(void)
         tft.println(Score);
         digitalWrite(11, HIGH);
 
-        Serial.begin(9600);
         Serial.print("2");
-        Serial.end();
-        state = 2;
+        state = 4;
         break;
       }
       break;  
     }
-    case 2 :
+    case 4 :
     {
       back_btn.initButton(&tft,  160, 420, 100, 40, WHITE, CYAN, BLACK, "BACK", 2);
       back_btn.drawButton(false);
-      state = 3;
+      state = 5;
       break;   
     }
 
-    case 3:
+    case 5:
     {
       bool down = Touch_getXY();
       back_btn.press(down && back_btn.contains(pixel_x, pixel_y));
